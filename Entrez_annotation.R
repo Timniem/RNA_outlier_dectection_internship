cb69f6b3e86892b190fcd5f6242d8963dfeed326
Timniem <144884985+Timniem@users.noreply.github.com>
2023-09-21 13:13:40 +0200

    Update Entrez_annotation.R

--- Entrez_annotation.R
+++ Entrez_annotation.R
@@ -4,14 +4,42 @@
 
 library("AnnotationDbi")
 library("org.Hs.eg.db")
+library("data.table")
+
 args <- commandArgs(trailingOnly = TRUE)
 cts_table <- read.table(args[1],header=TRUE, sep="\t")
-cts_table$Geneid <- unlist(lapply(strsplit(as.character(cts_table$Geneid), "[.]"), '[[', 1))
-cts_table$entrez = mapIds(org.Hs.eg.db,
-                    keys=cts_table$Geneid, #Column containing Ensembl gene ids
+
+#remove headers 2-6 
+cts_table <- cts_table[,c(1,7:ncol(cts_table))]
+Ensembl_stripped <- unlist(lapply(strsplit(as.character(cts_table$Geneid), "[.]"), '[[', 1))
+
+cts_table$EntrezID = mapIds(org.Hs.eg.db,
+                    keys=Ensembl_stripped, #Vector containing Ensembl gene ids
                     column="ENTREZID",
                     keytype="ENSEMBL",
                     multiVals="first")
+
+cts_table$GeneSymbol = mapIds(org.Hs.eg.db,
+                    keys=Ensembl_stripped, 
+                    column="SYMBOL",
+                    keytype="ENSEMBL",
+                    multiVals="first")
+
+#Rename GeneID to EnsemblID
+colnames(cts_table)[colnames(cts_table) == 'Geneid'] <- 'EnsemblID'
+
+# Forced order with Ensembl, Entrez and Symbol first
 columns <- colnames(cts_table)
-cts_table <- cts_table[,c("entrez",columns[columns != "entrez"])]
-write.table(cts_table,  file = args[1], sep ="\t" ,row.names = FALSE, col.names = TRUE)
+last_columns <- setdiff(columns, c("EnsemblID","EntrezID","GeneSymbol"))
+renamed_last_columns <- character()
+for (name in last_columns){
+    split_name <- strsplit(name, '\\.')[[1]]
+    joined_name <- paste(c(split_name[12:14]), collapse='_')
+    renamed_last_columns <- c(renamed_last_columns, joined_name)
+    }
+
+cts_table <- cts_table[,c("EnsemblID","EntrezID","GeneSymbol", last_columns)]
+
+setnames(cts_table, last_columns, renamed_last_columns)
+
+write.table(cts_table,  file = args[2], sep ="\t" ,row.names = FALSE, col.names = TRUE)
