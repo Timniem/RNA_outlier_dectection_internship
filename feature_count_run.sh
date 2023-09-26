b9c0b179350d41d62dc14d4cdbe5a91c66344fba
Timniem <144884985+Timniem@users.noreply.github.com>
2023-09-26 15:21:50 +0200

    Update feature_count_run.sh
    update 2609

--- feature_count_run.sh
+++ feature_count_run.sh
@@ -2,12 +2,14 @@
 #SBATCH --job-name=feature_count_ps_test
 #SBATCH --output=feature_count_test_ps.out
 #SBATCH --error=feature_count_test_ps.err
-#SBATCH --time=20:00:01
-#SBATCH --cpus-per-task=8
+#SBATCH --time=00:10:01
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=16gb
 #SBATCH --nodes=1
 #SBATCH --export=NONE
 #SBATCH --get-user-env=60L
+
+# featureCounts
 
 # Loading environment
 eval "$(conda shell.bash hook)"
@@ -20,22 +22,18 @@ converted_matrix_path="${output_path%.txt}.Rmatrix.txt"
 input_path=/groups/umcg-gdio/tmp01/umcg-kmaassen/samples/rnaseq/blood
 annot_path=/groups/umcg-gdio/tmp01/umcg-kmaassen/resources/gtf/gencode.v34lift37.annotation.gtf
 
-# change directory to input folder
-cd $input_path
-
 # feature counts -p = paired end data=True, -s 1 = forward stranded? -a = annotation file
-# -T 8 = number of threads used, which is 8
 # -o output file, followed by the input BAM(s)
 featureCounts -p -s 1 -T 8 \
     -a $annot_path \
     -o $output_path $input_path/*.bam
 
-# Remove unnecessary headers 
-cut -f1,7,8,9,10,11,12 \
-    $output_path \
-    > $converted_matrix_path
-
 mamba deactivate
-# Running R script for Entrez annotation
+
+
+# Running R script for Entrez annotation and remove unnecessary headers 
+
 mamba activate r-notebook-kernel
-Rscript /groups/umcg-gdio/tmp01/umcg-tniemeijer/featurecounts/Entrez_annotation.R $converted_matrix_path
+# First argument = path of the txt file from featureCount, second argument = the desired output path
+Rscript /groups/umcg-gdio/tmp01/umcg-tniemeijer/featurecounts/format_count_file.R $output_path $converted_matrix_path
+
