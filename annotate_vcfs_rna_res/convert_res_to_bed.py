696740f52fff24ae70d20f2b02733cf5ab89ba5d
Timniem <t.niemeijer@umcg.nl>
2024-01-24 15:20:41 +0000

    made compatible with newest layout, added 'mode' to the output. Fixed annotation columns for MAE

--- annotate_vcfs_rna_res/convert_res_to_bed.py
+++ annotate_vcfs_rna_res/convert_res_to_bed.py
@@ -22,38 +22,6 @@ def expand_range(res, region_range=10):
     res.end = res.end + (region_range)
     return res
 
-
-def format_res(res, pcutoff=0.5, mode):
-    """
-    Formatting results from either FRASER, OUTRIDER or MAE
-    -------------------------------------
-    input:
-        results file (.tsv)
-        p_cutoff (float)
-        mode (string)
-    output:
-        df with .bed like formatted data. (pd.Dataframe)
-    """
-    res = res.dropna() # drop NA's for now only support for known Entrez genes.
-    res = res[res.padjust < pcutoff] 
-    chrdict = {"1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8,
-            "9":9, "10":10, "11":11, "12":12, "13":13, "14":14,
-            "15":15, "16":16, "17":17, "18":18, "19":19, "20":20,
-            "21":21, "22":22, "X":23, "Y":24}
-    res["chrindex"] = [chrdict[x] for x in res.seqnames]
-    res = res.sort_values(["chrindex","start"])
-    res = res.reset_index().drop(columns=["index","chrindex"])
-    match mode:
-        case "FRASER":
-            res = res[["chr","start","end","padjust", "deltaPsi"]]
-            res = expand_range(res, 10)
-        case "OUTRIDER":
-            res = res[["chr","start", "end", "padjust", "zScore"]]
-        case "MAE":
-            res = res[["chr","start", "end", "padj", "log2FC"]]
-    return res
-
-
 def check_res_type(res):
     """
     Checks presence of type specific columns in dataframe.
@@ -67,12 +35,45 @@ def check_res_type(res):
     else: 
         return "MAE"
 
+
+
+def format_res(res, pcutoff=0.5):
+    """
+    Formatting results from either FRASER, OUTRIDER or MAE
+    -------------------------------------
+    input:
+        results file (.tsv)
+        p_cutoff (float)
+        mode (string)
+    output:
+        df with .bed like formatted data. (pd.Dataframe)
+    """
+    res = res.dropna() # drop NA's for now only support for known Entrez genes.
+    res = res[res.chr != 'MT']
+    res = res[res.padjust < pcutoff] 
+    chrdict = {"1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8,
+            "9":9, "10":10, "11":11, "12":12, "13":13, "14":14,
+            "15":15, "16":16, "17":17, "18":18, "19":19, "20":20,
+            "21":21, "22":22, "X":23, "Y":24}
+    res["chrindex"] = [chrdict[x] for x in res.chr]
+    res = res.sort_values(["chrindex","start"])
+    res = res.reset_index().drop(columns=["index","chrindex"])
+    mode = check_res_type(res)
+    match mode:
+        case "FRASER":
+            res = res[["chr","start","end","padjust", "deltaPsi"]]
+            res = expand_range(res, 10)
+        case "OUTRIDER":
+            res = res[["chr","start", "end", "padjust", "zScore"]]
+        case "MAE":
+            res = res[["chr","start", "end", "padjust", "log2FC"]]
+    return res, mode
+
 def main():
     args = sys.argv
-    results = pd.read_csv(args[1], sep='\t')
-    results = format_res(results)
-    results = expand_range(results)
-    results.to_csv(path_or_buf=args[2], header=False, sep="\t", index=False)
+    results = pd.read_csv(args[1], sep="\t")
+    results, mode = format_res(results)
+    results.to_csv(path_or_buf=f"{mode}_results.bed", header=False, sep="\t", index=False)
 
 if __name__ == "__main__":
     main()
