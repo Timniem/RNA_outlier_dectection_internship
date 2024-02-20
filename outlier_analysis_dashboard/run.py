08a825db5d6251ac2dfbd4250e15c379a77d65fb
Timniem <t.niemeijer@umcg.nl>
2024-02-20 12:52:42 +0000

    feat: update dashboard, command line args

--- outlier_analysis_dashboard/run.py
+++ outlier_analysis_dashboard/run.py
@@ -3,13 +3,12 @@ import pandas as pd
 import panel as pn
 from bokeh.io import output_notebook, push_notebook, show, save, output_file
 from bokeh.resources import INLINE
-
-
+import sys
 
 def main():
-    fraser_data = pd.read_csv("DATA/result_table_fraser.tsv", sep='\t')
-    outrider_data = pd.read_csv("DATA/result_table_outrider.tsv", sep='\t')
-    mae_data = pd.read_csv("DATA/result_table_mae.tsv", sep='\t')
+    fraser_data = pd.read_csv(sys.argv[1], sep='\t')
+    outrider_data = pd.read_csv(sys.argv[2], sep='\t')
+    mae_data = pd.read_csv(sys.argv[3], sep='\t')
     dashboard = Dashboard(fraser_data, outrider_data, mae_data).run()
     dashboard.servable()
 main()
--- outlier_analysis_dashboard/run_dashboard.sh
+++ outlier_analysis_dashboard/run_dashboard.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -p|--port)
+            PORT="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -d|--data)
+            DATA="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 eval "$(conda shell.bash hook)"
 source /groups/umcg-gdio/tmp01/umcg-tniemeijer/envs/mamba-env/etc/profile.d/mamba.sh
 mamba activate dashboard_env
 
-panel serve run.py --port 7979
+fraser_data="${DATA}/*fraser.tsv"
+outrider_data="${DATA}/*outrider.tsv"
+mae_data="${DATA}/*mae.tsv"
+
+panel serve run.py --port $PORT --args $fraser_data $outrider_data $mae_data 
